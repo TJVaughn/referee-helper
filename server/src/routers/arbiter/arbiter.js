2c3459a48eb5662ef4c3c0ba0daa445c19cdf41d
Trevor <hauck.trevor@gmail.com>
2020-02-22 01:04:43 -0500

    Added puppeteer to project, Now I can grab the schedule from arbiter sports. I am currently working on formatting the information, and removing unnecessary bits. So the front end will provide form data, and then the backend will then populate the database with the game data if the response is successful

--- server/src/routers/arbiter/arbiter.js
+++ server/src/routers/arbiter/arbiter.js
@@ -1,26 +1,58 @@
 const express = require('express')
 const router = new express.Router()
 const https = require('https')
+const puppeteer = require('puppeteer')
+
+const getArbiterSchedule = async (email, pass) => {
+    const browser = await puppeteer.launch({headless: true})
+    const page = await browser.newPage()
+    await page.setViewport({
+        width: 2000,
+        height: 1500,
+        deviceScaleFactor: 1
+    })
+    await page.goto('https://www1.arbitersports.com/shared/signin/signin.aspx');
+    await page.click('#txtEmail')
+    await page.keyboard.type(email)
+    await page.click('#txtPassword')
+    await page.keyboard.type(pass)
+    await page.click('#ctl00_ContentHolder_pgeSignIn_conSignIn_btnSignIn')
+    await page.content()
+    await page.click('#mobileAlertStayLink')
+    await page.goto('https://www1.arbitersports.com/Official/GameScheduleEdit.aspx')
+    await page.content()
+    await page.click('tr.alternatingItems:nth-child(7)')
+    await page.content()
+    await page.goto('https://www1.arbitersports.com/Official/GameScheduleEdit.aspx')
+    await page.setRequestInterception(true)
+    page.on('request', (request) => {
+        if(request.resourceType == 'stylesheet'){
+            request.abort()
+        } else {
+            request.continue()
+        }
+    })
+    return await page.content()
+}
 
 router.get('/api/arbiter/schedule', async (req, res) => {
-    const url = new URL('https://www1.arbitersports.com/Official/GameScheduleEdit.aspx')
-    const newUrl = new URL('https://www1.arbitersports.com/shared/signin/signin.aspx?RedirectUrl=/Official/GameScheduleEdit.aspx')
-    const options = {
-        auth: 'hauck.trevor@gmail.com:aLivenFree@88!',
-        method: 'POST'
-    }
-    const request = https.request(newUrl, options, (response) => {
-        console.log("Status: \n", response.statusCode)
-        console.log("Headers: \n", response.headers)
-
-        response.on('data', (d) => {
-            res.send(d)
-        })
+    const userEmail = req.body.email
+    const userPass = req.body.password
+    let htmlSchedule = await getArbiterSchedule(userEmail, userPass)
+    htmlSchedule = htmlSchedule.toString()
+    htmlSchedule = htmlSchedule.toLowerCase().split('>').join('').split('<').join('').split(' ')
+    // htmlSchedule = htmlSchedule
+    // for(let i = 0; i < htmlSchedule.length; i++){
+    //     if(i === '<'){
+    //         htmlSchedule = htmlSchedule.splice(i, 2)
+    //     }
+    // }
+    // res.send(htmlSchedule)
+    let filteredSchedule = htmlSchedule.filter((item) => {
+        return item
     })
-    request.on("error", (err) => {
-        console.log(err)
-    })
-    request.end()
+    res.send(filteredSchedule)
+    // res.send(htmlSchedule)
 })
 
 module.exports = router
