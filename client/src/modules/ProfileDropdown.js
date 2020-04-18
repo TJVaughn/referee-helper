e1dfb688dee68c68e5ecfc26d9bd0d175cd9eabe
Trevor <hauck.trevor@gmail.com>
2020-04-18 10:51:23 -0400

    Styled the profile button and dropdown.

--- client/src/modules/ProfileDropdown.js
+++ client/src/modules/ProfileDropdown.js
@@ -5,9 +5,12 @@ class ProfileDropdown extends Component {
     constructor(props){
         super(props)
         this.state = {
-            toggle: false
+            toggle: false,
+            logout: false
         }
         this.handleToggle = this.handleToggle.bind(this)
+        this.handleLogout = this.handleLogout.bind(this)
+        this.logoutUser = this.logoutUser.bind(this)
     }
     handleToggle(){
         if(this.state.toggle){
@@ -15,16 +18,31 @@ class ProfileDropdown extends Component {
         }
         this.setState({toggle: true})
     }
+    handleLogout() {
+        if(this.state.logout){
+            return this.setState({logout: false})
+        }
+        this.setState({logout: true})
+    }
+    logoutUser(){
+
+    }
     render(){
     	return(
-    		<div>
+    		<div className="Profile-dropdown-container">
                 <img onClick={this.handleToggle} className="Profile-img-icon" alt='profile' src={ProfileImg} />
                 
                 {this.state.toggle
                 ?<div className={`Profile-img-dropdown`}>
-                    <Link to={'/profile'}>Account</Link>
+                    <Link onClick={this.handleToggle} to={'/profile'}>Account</Link>
                     <br />
-                    <Link>Log Out</Link>
+                    {this.state.logout
+                    ?<div>
+                        <span className="pointer" onClick={this.handleLogout}>Are you sure?</span><br />
+                        <Link onClick={this.logoutUser}>Logout</Link>
+                    </div>
+                    :<Link onClick={this.handleLogout}>Log Out</Link>}
+                    
                 </div>
                 :''}
     		</div>
