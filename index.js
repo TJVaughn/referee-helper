f8f9a5e8fa4c8d836c7c44604da1511c4c7e212a
Trevor <hauck.trevor@gmail.com>
2020-04-28 13:15:47 -0400

    Forgot to require path

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const port = process.env.PORT
+const path = require('path')
 
 const app = require('./server/src/app')
 // IMPORT ROUTERS ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
